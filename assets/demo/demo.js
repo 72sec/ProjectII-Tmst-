4aba203e204f924ba5793b37b7a7ea76a4f07c9d
[Your <aom.aun.monthiraj@gmail.com>
2020-04-26 18:45:18 +0700

    Update theme & data 03

---
+++ assets/demo/demo.js
@@ -0,0 +1,269 @@
+
+demo = {
+    initPickColor: function(){
+        $('.pick-class-label').click(function(){
+            var new_class = $(this).attr('new-class');
+            var old_class = $('#display-buttons').attr('data-class');
+            var display_div = $('#display-buttons');
+            if(display_div.length) {
+            var display_buttons = display_div.find('.btn');
+            display_buttons.removeClass(old_class);
+            display_buttons.addClass(new_class);
+            display_div.attr('data-class', new_class);
+            }
+        });
+    },
+    initDocChart: function(){
+      chartColor = "#FFFFFF";
+
+      // General configuration for the charts with Line gradientStroke
+      gradientChartOptionsConfiguration = {
+          maintainAspectRatio: false,
+          legend: {
+              display: false
+          },
+          tooltips: {
+            bodySpacing: 4,
+            mode:"nearest",
+            intersect: 0,
+            position:"nearest",
+            xPadding:10,
+            yPadding:10,
+            caretPadding:10
+          },
+          responsive: true,
+          scales: {
+              yAxes: [{
+                display:0,
+                gridLines:0,
+                ticks: {
+                    display: false
+                },
+                gridLines: {
+                    zeroLineColor: "transparent",
+                    drawTicks: false,
+                    display: false,
+                    drawBorder: false
+                }
+              }],
+              xAxes: [{
+                display:0,
+                gridLines:0,
+                ticks: {
+                    display: false
+                },
+                gridLines: {
+                  zeroLineColor: "transparent",
+                  drawTicks: false,
+                  display: false,
+                  drawBorder: false
+                }
+              }]
+          },
+          layout:{
+            padding:{left:0,right:0,top:15,bottom:15}
+          }
+      };
+
+   
+    },
+
+    initDashboardPageCharts: function(all,alldeaths,allre){
+
+      chartColor = "#FFFFFF";
+      // General configuration for the charts with Line gradientStroke
+      gradientChartOptionsConfiguration = {
+          maintainAspectRatio: false,
+          legend: {
+              display: false
+          },
+          tooltips: {
+            bodySpacing: 4,
+            mode:"nearest",
+            intersect: 0,
+            position:"nearest",
+            xPadding:10,
+            yPadding:10,
+            caretPadding:10
+          },
+          responsive: 1,
+          scales: {
+              yAxes: [{
+                display:0,
+                gridLines:0,
+                ticks: {
+                    display: false
+                },
+                gridLines: {
+                    zeroLineColor: "transparent",
+                    drawTicks: false,
+                    display: false,
+                    drawBorder: false
+                }
+              }],
+              xAxes: [{
+                display:0,
+                gridLines:0,
+                ticks: {
+                    display: false
+                },
+                gridLines: {
+                  zeroLineColor: "transparent",
+                  drawTicks: false,
+                  display: false,
+                  drawBorder: false
+                }
+              }]
+          },
+          layout:{
+            padding:{left:0,right:0,top:15,bottom:15}
+          }
+      };
+
+      gradientChartOptionsConfigurationWithNumbersAndGrid = {
+          maintainAspectRatio: false,
+          legend: {
+              display: false
+          },
+          tooltips: {
+            bodySpacing: 4,
+            mode:"nearest",
+            intersect: 0,
+            position:"nearest",
+            xPadding:10,
+            yPadding:10,
+            caretPadding:10
+          },
+          responsive: true,
+          scales: {
+              yAxes: [{
+                gridLines:0,
+                gridLines: {
+                    zeroLineColor: "transparent",
+                    drawBorder: false
+                }
+              }],
+              xAxes: [{
+                display:0,
+                gridLines:0,
+                ticks: {
+                    display: false
+                },
+                gridLines: {
+                  zeroLineColor: "transparent",
+                  drawTicks: false,
+                  display: false,
+                  drawBorder: false
+                }
+              }]
+          },
+          layout:{
+            padding:{left:0,right:0,top:15,bottom:15}
+          }
+      };
+
+
+
+
+      ctx = document.getElementById('Rocovered').getContext("2d");
+
+      gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+      gradientStroke.addColorStop(0, '#18ce0f');
+      gradientStroke.addColorStop(1, chartColor);
+
+      gradientFill = ctx.createLinearGradient(0, 170, 0, 50);
+      gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
+      gradientFill.addColorStop(1, hexToRGB('#18ce0f',0.4));
+
+      myChart = new Chart(ctx, {
+          type: 'line',
+          responsive: true,
+          data: {
+              labels: ["12pm,", "3pm",  "9am"],
+              datasets: [{
+                  label: "Email Stats",
+                  borderColor: "#18ce0f",
+                  pointBorderColor: "#FFF",
+                  pointBackgroundColor: "#18ce0f",
+                  pointBorderWidth: 2,
+                  pointHoverRadius: 4,
+                  pointHoverBorderWidth: 1,
+                  pointRadius: 4,
+                  fill: true,
+                  backgroundColor: gradientFill,
+                  borderWidth: 2,
+                  data: [0,allre]
+              }]
+          },
+          options: gradientChartOptionsConfigurationWithNumbersAndGrid
+      });
+
+      ctx = document.getElementById('Deaths').getContext("2d");
+
+      gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+      gradientStroke.addColorStop(0, '#f60505');
+      gradientStroke.addColorStop(1, chartColor);
+
+      gradientFill = ctx.createLinearGradient(0, 170, 0, 50);
+      gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
+      gradientFill.addColorStop(1, hexToRGB('#f60505',0.4));
+
+      myChart = new Chart(ctx, {
+          type: 'line',
+          responsive: true,
+          data: {
+              labels: ["12pm","3pm", "9am"],
+              datasets: [{
+                  label: "Email Stats",
+                  borderColor: "#e74545",
+                  pointBorderColor: "#f4b1b1",
+                  pointBackgroundColor: "#e74545",
+                  pointBorderWidth: 2,
+                  pointHoverRadius: 4,
+                  pointHoverBorderWidth: 1,
+                  pointRadius: 4,
+                  fill: true,
+                  backgroundColor: gradientFill,
+                  borderWidth: 2,
+                  data: [0,alldeaths]
+              }]
+          },
+          options: gradientChartOptionsConfigurationWithNumbersAndGrid
+      });
+      ctx = document.getElementById('Comfermed').getContext("2d");
+
+      gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+      gradientStroke.addColorStop(0, '#ffc100');
+      gradientStroke.addColorStop(1, chartColor);
+
+      gradientFill = ctx.createLinearGradient(0, 170, 0, 50);
+      gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
+      gradientFill.addColorStop(1, hexToRGB('#ffc100',0.4));
+
+      myChart = new Chart(ctx, {
+          type: 'line',
+          responsive: true,
+          data: {
+              labels: ["12pm,","3pm", "9am"],
+              datasets: [{
+                  label: "Email Stats",
+                  borderColor: "#ffc100",
+                  pointBorderColor: "#f0eec6",
+                  pointBackgroundColor: "#ffc100",
+                  pointBorderWidth: 2,
+                  pointHoverRadius: 4,
+                  pointHoverBorderWidth: 1,
+                  pointRadius: 4,
+                  fill: true,
+                  backgroundColor: gradientFill,
+                  borderWidth: 2,
+                  data: [0,all]
+              }]
+          },
+          options: gradientChartOptionsConfigurationWithNumbersAndGrid
+      });
+      $("#all").append(all);
+        $("#allre").append(allre);
+        $("#alldeaths").append(alldeaths);
+    },
+};
